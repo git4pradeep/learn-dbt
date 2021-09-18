db36de57fb66268315331b52aec3ee233971be3d
pkothakota@luxoft.com <pkothakota@luxoft.com>
2021-09-18 20:32:32 +0000

    added test cases , other stuff

--- models/dim_customers.sql
+++ models/dim_customers.sql
@@ -1,23 +1,12 @@
-with customers as (
+  with customers as (
 
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from analytics.jaffle_shop.customers
+    select * from {{ ref('stg_customers')}}
 
 ),
 
 orders as (
 
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from analytics.jaffle_shop.orders
+    select * from {{ ref('stg_orders') }}
 
 ),
 
